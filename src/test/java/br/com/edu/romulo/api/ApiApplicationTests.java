37adf8d0664d5384f6e57de43852a17e69c2999a
javaExactus <romulo.neves@exactus.com.br>
2025-03-26 19:16:31 -0300

    primeiro commit

---
+++ src/test/java/br/com/edu/romulo/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edu.romulo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
